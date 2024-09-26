71763037500e278bbaffd32560556f4e35c1ed85
mahmoud-ramadn <mr9026573@gmail.com>
2024-09-26 03:36:06 +0300

    e

--- front/src/pages/Orders.tsx
+++ front/src/pages/Orders.tsx
@@ -3,7 +3,7 @@ import { Lodaing } from "@components/feedback/Loading";
 
 import Heading from "@components/common/Heading/Heading";
 
-import FullDAta from "@components/eCommerce/FullData/FullDAta";
+import FullDAta from "../components/ecommerce/FullData/FullDAta";
 import { Table, Modal } from "react-bootstrap";
 // khfjkhdskafasdhfkjas
 
